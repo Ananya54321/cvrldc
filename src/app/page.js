e5a14567b6809a234b559c31ba8cc0e3a215e4de
Ananya54321 <ananyapappula521@gmail.com>
2025-02-20 20:43:54 +0530

    revert

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>hello</div>;
+  return (
+    <div className="h-screen flex flex-col text-4xl font-bold justify-center items-center">
+      <p>Literary and Debate Club, CVR</p>
+      <br />
+      <br />
+      <p>Coming soon! {":)"}</p>
+    </div>
+  );
 };
 
 export default page;
