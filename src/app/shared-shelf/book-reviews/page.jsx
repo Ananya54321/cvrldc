aecf11d9c70d4e510992437bb7959364de0963bf
Ananya54321 <ananyapappula521@gmail.com>
2025-03-10 23:31:37 +0530

    ui update

---
+++ src/app/shared-shelf/book-reviews/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>post your book review</div>;
+};
+
+export default page;
--- src/components/pages/events/FilteredEvents.jsx
+++ src/components/pages/events/FilteredEvents.jsx
@@ -42,7 +42,7 @@ function FilteredEvents({ vertical }) {
   // Split events into upcoming & past
   const upcomingEvents = events
     .filter((event) => new Date(event.eventDate) >= now)
-    .sort((a, b) => new Date(a.eventDate) - new Date(b.eventDate)); 
+    .sort((a, b) => new Date(a.eventDate) - new Date(b.eventDate));
 
   const pastEvents = events
     .filter((event) => new Date(event.eventDate) < now)
@@ -50,7 +50,9 @@ function FilteredEvents({ vertical }) {
 
   const renderEvents = (eventsList, title) => (
     <div>
-      <h2 className="text-4xl font-bold mb-6 text-center">{title}</h2>
+      <h2 className=" font-bold text-5xl titlefont my-6 md:my-16 text-center">
+        {title}
+      </h2>
       <div className="flex flex-col gap-8 items-center">
         {eventsList.length > 0 ? (
           eventsList.map((event, index) => {
@@ -64,8 +66,7 @@ function FilteredEvents({ vertical }) {
             return (
               <div
                 key={index}
-                className="flex flex-col md:flex-row md:w-[70%] bg-ternary rounded-xl shadow-lg overflow-hidden"
-              >
+                className="flex flex-col w-[100%] md:flex-row md:w-[70%] bg-ternary rounded-xl shadow-lg overflow-hidden">
                 <div className="md:w-1/3 h-32 md:h-60">
                   <img
                     src={imageSrc}
@@ -74,14 +75,14 @@ function FilteredEvents({ vertical }) {
                   />
                 </div>
 
-                <div className="md:w-2/3 p-6 flex flex-col md:justify-between">
+                <div className="w-[100%] md:w-2/3 p-6 flex flex-col md:justify-between">
                   <h3 className="text-lg md:text-2xl font-bold text-accent mb-2">
                     {event.title}
                   </h3>
                   <div className="flex items-center gap-2 text-xs md:text-sm text-white/80 mb-2">
-                    <span className="md:mr-4 bg-primary/20 text-center rounded-lg p-1 md:p-2">
+                    <span className="md:mr-4 bg-primary/20 text-center rounded-lg p-2">
                       {new Date(event.eventDate).toLocaleString("en-US", {
-                        weekday: "short",
+                        // weekday: "short",
                         year: "numeric",
                         month: "short",
                         day: "2-digit",
@@ -91,7 +92,7 @@ function FilteredEvents({ vertical }) {
                       })}
                     </span>
 
-                    <span className="bg-primary/20 text-center rounded-lg p-1 md:p-2">
+                    <span className="bg-primary/20 text-center rounded-lg p-2">
                       {event.location}
                     </span>
                   </div>
@@ -109,8 +110,7 @@ function FilteredEvents({ vertical }) {
                         href={event.link}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="bg-accent text-primary py-1 px-4 rounded-lg hover:bg-[#e5a970] transition-all font-medium"
-                      >
+                        className="bg-accent text-primary py-1 px-4 rounded-lg hover:bg-[#e5a970] transition-all font-medium">
                         Visit Event
                       </a>
                     ) : (
@@ -136,15 +136,14 @@ function FilteredEvents({ vertical }) {
 
   return (
     <div
-      className="bg-secondary rounded-3xl text-primary py-6 md:py-16 px-1 md:px-8"
-      id="events"
-    >
+      className="bg-secondary rounded-3xl text-primary px-1 md:px-8"
+      id="events">
       <div className="md:max-w-6xl mx-auto">
-        <h2 className="text-5xl titlefont mb-6 md:mb-16 text-center">
+        {/* <h2 className="text-5xl titlefont mb-6 md:mb-16 text-center">
           Our Latest Events
-        </h2>
+        </h2> */}
         {renderEvents(upcomingEvents, "Upcoming Events")}
-        <div className="mt-10">{renderEvents(pastEvents, "Past Events")}</div>
+        <div>{renderEvents(pastEvents, "Past Events")}</div>
       </div>
     </div>
   );
